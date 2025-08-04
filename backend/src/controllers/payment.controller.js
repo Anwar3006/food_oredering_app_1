f31a5f97c20c9cf12c92896f06ca095846bfe852
Anwar3006 <anwarsadat.d2@gmail.com>
2025-08-04 21:24:14 +0000

    chore: added webhooks for stripe events - 2

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -94,6 +94,8 @@ export const PaymentController = {
       return res.status(400).send(`Webhook Error: ${err.message}`);
     }
 
+    console.log(`Received event: ${event.type}`);
+
     // Handle the event
     switch (event.type) {
       case "payment_intent.succeeded":
